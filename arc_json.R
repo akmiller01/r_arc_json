0aa26d55eb388d3e15a5d41341725f7e6bc5c210
Alex Miller <alex.k.miller@gmail.com>
2018-07-13 08:34:49 -0400

    Done!

--- arc_json.R
+++ arc_json.R
@@ -1,8 +1,73 @@
-library(jsonlite)
+list.of.packages <- c("jsonlite","data.table","sp","rgdal","rgeos","spdep")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
 
-url = "https://gis.dhsprogram.com/arcgis/rest/services/Geometry/SDR_Regions/MapServer/0/query?f=json&where=DHSCC+in+%28%27ET%27%29+AND+SVYTYPE+in+%28%27DHS%27%29&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=DHSCC%2CCNTRYNAMEE%2CSVYTYPE%2CSVYYEAR%2CDHSREGEN%2CREGNOTES%2COBJECTID%2CSVYNOTES%2CSVYID%2CLEVELRNK%2CLEVELNA%2CColorAdjust%2CSvy_Map%2CSTATCOM%2CNATL_DATA&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=SVYYEAR%2CSVYTYPE%2CSVYID%2CLEVELRNK&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&f=html"
+ring2poly = function(ring){
+  ring.dim = dim(ring)
+  if(length(ring.dim)==3){
+    point.len = ring.dim[2]
+    x1 = ring[1,c(1:point.len),1]
+    y1 = ring[1,c(1:point.len),2] 
+  }else{
+    point.len = ring.dim[1]
+    x1 = ring[c(1:point.len),1]
+    y1 = ring[c(1:point.len),2]
+  }
+  c1 = cbind(x1, y1)
+  r1 = rbind(c1, c1[1, ])
+  P1 = Polygon(r1)
+  return(P1)
+}
 
-arc_json_data = fromJSON(url)
-x = arc_json_data["features"][[1]][["geometry"]][["rings"]][[1]][c(1:7156)]
-y = arc_json_data["features"][[1]][["geometry"]][["rings"]][[1]][c(7157:14312)]
-plot(x,y)
+arcJson = function(url){
+  arc_json_data = fromJSON(url)
+  poly.list = list()
+  attrib.list = list()
+  for(i in 1:length(arc_json_data["features"][[1]][["geometry"]][["rings"]])){
+    ring = arc_json_data["features"][[1]][["geometry"]][["rings"]][[i]]
+    attribs = arc_json_data['features'][[1]]["attributes"][[1]][i,]
+    attrib.list[[i]] = attribs
+    if(typeof(ring)=="double"){
+      P1 = ring2poly(ring)
+      Ps1 = Polygons(list(P1), ID = attribs["OBJECTID"][[1]])
+      poly.list[[i]] = Ps1
+    }else{
+      subpoly.list = list()
+      for(j in 1:length(ring)){
+        subring = ring[[j]]
+        P1 = ring2poly(subring)
+        subpoly.list[[j]] = P1
+      }
+      Ps1 = Polygons(subpoly.list, ID = attribs["OBJECTID"][[1]])
+      poly.list[[i]] = Ps1
+    }
+  }
+  poly.dat = rbindlist(attrib.list)
+  rownames(poly.dat) = poly.dat$OBJECTID
+  SPs = SpatialPolygons(poly.list)
+  SPDF = SpatialPolygonsDataFrame(SPs,poly.dat)
+  crs.geo <- CRS("+proj=longlat +datum=WGS84")
+  proj4string(SPDF) <- crs.geo
+  return(SPDF)
+}
+
+dhsQuery = function(query){
+  query = gsub(" ","+",query)
+  query = gsub("'","%27",query)
+  query = gsub("\\(","%28",query)
+  query = gsub(")","%29",query)
+  url = paste0(
+    "https://gis.dhsprogram.com/arcgis/rest/services/Geometry/SDR_Regions/MapServer/0/query?",
+    "f=json",
+    "&where=",query,
+    "&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=DHSCC%2CCNTRYNAMEE%2CSVYTYPE%2CSVYYEAR%2CDHSREGEN%2CREGNOTES%2COBJECTID%2CSVYNOTES%2CSVYID%2CLEVELRNK%2CLEVELNA%2CColorAdjust%2CSvy_Map%2CSTATCOM%2CNATL_DATA&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=SVYYEAR%2CSVYTYPE%2CSVYID%2CLEVELRNK&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=")
+  SPDF = arcJson(url)
+  return(SPDF)
+}
+
+# Example
+# eg.query = "DHSCC in ('AF') AND SVYTYPE in ('DHS') AND SVYYEAR > 2010"
+# afg.dhs = dhsQuery(eg.query)
+rm(list.of.packages)
+rm(new.packages)
